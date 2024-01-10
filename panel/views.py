48d2672b8f1a8a02aa512efff0760a8071f78d99
solaleh81 <solalehalizadeh81.com>
2024-01-10 17:39:25 +0330

    add: def dashboard.

--- panel/views.py
+++ panel/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def test(request):
     return render(request, 'panel/base.html')
+
+def dashboard(request):
+    return render(request, 'panel/dashboard.html')
