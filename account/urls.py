f95a1b5fbc260cf68fd39cb9d6c7d91bee1ad642
solaleh81 <solalehalizadeh81.com>
2024-01-12 12:16:29 +0330

    add: سهلsignup url.

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
-# # urls.py
-# from django.urls import path
-# from .views import signup
-#
-# urlpatterns = [
-#     path('', signup, name='home'),
-#     path('signup/', signup, name='signup'),
-#     # Add other URL patterns as needed
-# ]
+# urls.py
+from django.urls import path
+from .views import signup, home
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('signup/', signup, name='signup'),
+    # Add other URL patterns as needed
+]
