77da0a3c9964a5742ff1be2630b1e0593cb045d0
solaleh81 <solalehalizadeh81.com>
2024-01-12 12:57:53 +0330

    add: features field to category.

--- translate/models.py
+++ translate/models.py
@@ -4,5 +4,5 @@ from account.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
     caption = models.TextField()
-    price = models.IntegerField()
-    # features
+    price = models.IntegerField() # price for each word
+    features = models.JSONField()
