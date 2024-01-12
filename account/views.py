63c3af62b5102d6a6f4ac6cd02a3010cf6c27bfa
solaleh81 <solalehalizadeh81.com>
2024-01-12 12:16:51 +0330

    add: سÙdef signup and home.

--- account/views.py
+++ account/views.py
@@ -1,29 +1,33 @@
-# # views.py
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.models import User
-# from .forms import SignupForm
-#
-# def home(request):
-#     return render(request, 'account/index.html')
-#
-# def signup(request):
-#     if request.method == 'POST':
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             confirm_password = form.cleaned_data['confirm_password']
-#
-#             # Perform any additional validation here
-#
-#             # Create user if everything is valid
-#             user = User.objects.create_user(username, password=password)
-#             form.save()
-#             # You can customize this part based on your user model
-#
-#             # Redirect to a success page or login page
-#             return redirect('home')  # Change 'login' to your login page URL name
-#     else:
-#         form = SignupForm()
-#
-#     return render(request, 'account/signup.html', {'form': form})
+# views.py
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+
+def home(request):
+    return render(request, 'account/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            if not username:
+                messages.error( 'ttttttttttttttttttttttttttt')
+            else:
+                user = form.save()
+                login(request, user)
+                return redirect('home')
+        else:
+            print(form.errors)
+
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'account/signup.html', {'form': form})
