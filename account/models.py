617a3ec88559974945e1abdb8aab8f46999ecf6b
solaleh81 <solalehalizadeh81.com>
2024-01-12 12:41:36 +0330

    add: User class.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 # In customusers/models.py
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+
+
+class User(AbstractBaseUser):
+    profile = models.URLField(null=True, blank=True)
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
