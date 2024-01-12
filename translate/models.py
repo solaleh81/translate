3e4d46bcf24e2232b1007974ab9c296c01775acb
solaleh81 <solalehalizadeh81.com>
2024-01-12 13:57:48 +0330

    add: Category model.

--- translate/models.py
+++ translate/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from account.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     caption = models.TextField()
     price = models.IntegerField() # price for each word
     features = models.JSONField()
+
+    def __str__(self):
+        return self.title
