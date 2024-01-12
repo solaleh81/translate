42e83e72210e3a4f4d33e1cb41e39cfbdaa581de
solaleh81 <solalehalizadeh81.com>
2024-01-12 13:56:57 +0330

    add: def index.

--- translate/views.py
+++ translate/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+def index(request):
+    categories = Category.objects.all()
+    return render(request, 'translate/index.html', {'categories': categories})
