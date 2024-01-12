46e17e40b56d11157b43c2caaf0629b9ce3d3ff5
solaleh81 <solalehalizadeh81.com>
2024-01-12 12:49:22 +0330

    add: Category class.

--- translate/models.py
+++ translate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    caption = models.TextField()
+    price = models.IntegerField()
+    # features
