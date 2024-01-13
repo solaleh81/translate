c1fb5aa1c46f0df23e33a2558264c9b38d9bb5c5
solaleh81 <solalehalizadeh81.com>
2024-01-13 22:42:13 +0330

    add: def service-1.

--- translate/views.py
+++ translate/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from .models import Product, Weblog
+from .models import Product, Weblog, Choice
+
 
 def index(request):
     products = Product.objects.all()
     weblogs = Weblog.objects.all()
     return render(request, 'translate/index.html', {'products': products, 'weblogs': weblogs})
+
+def service1(request):
+    products = Product.objects.all()
+    choices = Choice.objects.all()
+    return render(request, 'translate/service_1.html', {'products': products, 'choices': choices})
