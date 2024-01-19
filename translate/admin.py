8b40189cec11a58aa660dbf32ee10e0129742968
solaleh81 <solalehalizadeh81.com>
2024-01-19 14:28:44 +0330

    add: register ConditionType.

--- translate/admin.py
+++ translate/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Product, Weblog, Choice, TitleType, Order
+from .models import Product, Weblog, ChoiceOne, ChoiceTwo, TitleType, Order, ConditionType
 
 admin.site.register(Product)
 admin.site.register(Weblog)
-admin.site.register(Choice)
+admin.site.register(ChoiceOne)
+admin.site.register(ChoiceTwo)
 admin.site.register(TitleType)
 admin.site.register(Order)
+admin.site.register(ConditionType)
