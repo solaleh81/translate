fc72eb98d0953d9ff451fc48e4b9d3ff2d6ec2b5
solaleh81 <solalehalizadeh81.com>
2024-01-19 14:27:48 +0330

    add: create_order url.

--- translate/urls.py
+++ translate/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('service-1/', views.service1, name='service-1'),
+    path('success/', views.success, name='success'),
+    path('create_order/', views.create_order, name='create_order'),
 
 ]
