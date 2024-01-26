f3e47a4e8bb515bd49d0396ba90d2b18c375217d
solaleh81 <solalehalizadeh81.com>
2024-01-26 10:39:45 +0330

    add: login url.

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import signup, home
+from .views import signup, home, login_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
     # Add other URL patterns as needed
 ]
