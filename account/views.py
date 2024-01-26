79d1da2e09fc76e5ff9d6c69978ad99389545dd6
solaleh81 <solalehalizadeh81.com>
2024-01-26 10:39:09 +0330

    add: def login_view.

--- account/views.py
+++ account/views.py
@@ -11,20 +11,73 @@ from .forms import CustomUserCreationForm
 def home(request):
     return render(request, 'account/index.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'account/login.html')
+
+
+
+
+
+# def signup(request):
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             if username:
+#                 messages.add_message(request, messages.ERROR, 'نام کاربری وجود دارد.')
+#             # elif username == '':
+#             #     messages.add_message(request, messages.ERROR, 'نام کاربری را وارد کنید')
+#             # elif not username:
+#             #     messages.add_message(request, messages.ERROR, 'نام کاربری وجود ندارد.')
+#             else:
+#                 user = form.save()
+#                 login(request, user)
+#                 messages.add_message(request, messages.SUCCESS, 'ثبت نام با موفقیت انجام شد.')
+#                 # return redirect('home')
+#         else:
+#             print(form.errors)
+#
+#
+#     else:
+#         form = UserCreationForm()
+#
+#     return render(request, 'account/signup.html', {'form': form})
+
+
 
 def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
+
         if form.is_valid():
             username = form.cleaned_data['username']
-            if not username:
-                messages.error( 'ttttttttttttttttttttttttttt')
-            else:
-                user = form.save()
-                login(request, user)
-                return redirect('home')
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            user = form.save()
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'ثبت نام با موفقیت انجام شد.')
+            # if username:
+            #     messages.add_message(request, messages.ERROR, 'نام کاربری وجود دارد.')
+            # elif username == '':
+            #     messages.add_message(request, messages.ERROR, 'نام کاربری را وارد کنید')
+            # elif not username:
+            #     messages.add_message(request, messages.ERROR, 'نام کاربری وجود ندارد.')
+            if password1 and password2 and password1 != password2:
+               messages.add_message(request, messages.ERROR, 'رمز اول با رمز دوم برابر نیست!!!!')
+
         else:
+            messages.add_message(request, messages.ERROR, 'متاسفانه ثبت نام انجام نشد. مجددا امتحان کنید.')
             print(form.errors)
+                # return redirect('home')
+        # else:
+        #     print(form.errors)
 
 
     else:
