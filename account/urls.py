89eed952c520fe4b88121b1a048a6e0ce714bb7e
solaleh81 <solalehalizadeh81.com>
2024-01-26 12:41:26 +0330

    add: logout url.

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
-from .views import signup, home, login_view
+from .views import signup, home, login_view, logout_view
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('signup/', signup, name='signup'),
-    path('login/', login_view, name='login'),
+    path("", home, name="home"),
+    path("signup/", signup, name="signup"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
     # Add other URL patterns as needed
 ]
