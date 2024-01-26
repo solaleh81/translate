eee64d84aa9d5b404832c6122f2c06c3fbfb149f
solaleh81 <solalehalizadeh81.com>
2024-01-26 12:40:59 +0330

    add: def logout_view.

--- account/views.py
+++ account/views.py
@@ -2,27 +2,31 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 
 
 def home(request):
-    return render(request, 'account/index.html')
+    return render(request, "account/index.html")
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
-    return render(request, 'account/login.html')
+            return redirect("home")
+    return render(request, "account/login.html")
 
 
-
+def logout_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect("home")
 
 
 # def signup(request):
@@ -51,18 +55,19 @@ def login_view(request):
 #     return render(request, 'account/signup.html', {'form': form})
 
 
-
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
 
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
+            username = form.cleaned_data["username"]
+            password1 = form.cleaned_data["password1"]
+            password2 = form.cleaned_data["password2"]
             user = form.save()
             login(request, user)
-            messages.add_message(request, messages.SUCCESS, 'ثبت نام با موفقیت انجام شد.')
+            messages.add_message(
+                request, messages.SUCCESS, "ثبت نام با موفقیت انجام شد."
+            )
             # if username:
             #     messages.add_message(request, messages.ERROR, 'نام کاربری وجود دارد.')
             # elif username == '':
@@ -70,17 +75,22 @@ def signup(request):
             # elif not username:
             #     messages.add_message(request, messages.ERROR, 'نام کاربری وجود ندارد.')
             if password1 and password2 and password1 != password2:
-               messages.add_message(request, messages.ERROR, 'رمز اول با رمز دوم برابر نیست!!!!')
+                messages.add_message(
+                    request, messages.ERROR, "رمز اول با رمز دوم برابر نیست!!!!"
+                )
 
         else:
-            messages.add_message(request, messages.ERROR, 'متاسفانه ثبت نام انجام نشد. مجددا امتحان کنید.')
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "متاسفانه ثبت نام انجام نشد. مجددا امتحان کنید.",
+            )
             print(form.errors)
-                # return redirect('home')
+            # return redirect('home')
         # else:
         #     print(form.errors)
-
 
     else:
         form = UserCreationForm()
 
-    return render(request, 'account/signup.html', {'form': form})
+    return render(request, "account/signup.html", {"form": form})
